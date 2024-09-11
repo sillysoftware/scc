b7b28cef7e084d719715b13be8d64e6ef0b7abe7
vx-clutch <owestness@gmail.com>
2024-09-11 19:25:36 -0400

    final part of the saga of fixing my code

--- lisp/asm/asm.go
+++ lisp/asm/asm.go
@@ -12,7 +12,6 @@ import (
 	"fmt"
 	. "strconv"
 	. "strings"
-	"unicode"
 )
 
 var (
@@ -31,41 +30,8 @@ func arg(n int) string {
 	return fmt.Sprintf("${%d}", n)
 }
 
-func GenWordData(word string) string {
-	buf := data
-	wordids := []rune(word)
-	for i, char := range wordids {
-		if unicode.IsLetter(char) || unicode.IsDigit(char) {
-			continue
-		}
-		wordids[i] = '_'
-	}
-	wordid := string(wordids[:]) + Itoa(dataCounter)
-	wordidl := wordid
-	wordidl += "l"
-	buf = ReplaceAll(buf, arg(0), wordid)
-	buf = ReplaceAll(buf, arg(1), word)
-	if ContainsAny(word, "\n") {
-		buf = ReplaceAll(buf, "\n", "")
-		buf = ReplaceAll(buf, arg(2), ", 0xA")
-	} else {
-		buf = ReplaceAll(buf, arg(2), "")
-	}
-	buf = ReplaceAll(buf, arg(3), wordidl)
-	dataCounter++
-	return buf
-}
-
 func GenExit(status int) string {
 	buf := exit
 	buf = ReplaceAll(buf, arg(0), Itoa(status))
-	return buf
-}
-
-func GenWrite(desc int, word string, position int) string {
-	buf := write
-	buf = ReplaceAll(buf, arg(0), Itoa(desc))
-	buf = ReplaceAll(buf, arg(1), fmt.Sprintf("%s%d", word, position))
-	buf = ReplaceAll(buf, arg(2), fmt.Sprintf("%s%dl", word, position))
 	return buf
 }
--- lisp/asm/asm_test.go
+++ lisp/asm/asm_test.go
@@ -2,10 +2,6 @@ package asm_test
 
 import (
 	_ "embed"
-	"lisp/lisp/go/asm"
-	"lisp/lisp/go/types"
-	"lisp/lisp/go/cli"
-	"os"
 	"testing"
 )
 
@@ -15,16 +11,4 @@ var (
 )
 
 func TestAssembly(t *testing.T) {
-	var asmOut types.Assembly
-	asmOut.Init()
-	asmOut.AppendData(asm.GenWordData("lisp"))
-	asmOut.Append(asm.GenWrite(1, "lisp", 0))
-	asmOut.Reduce()
-	if asmOut.Operations != testAsset {
-		err := os.WriteFile("asmlog", []byte(asmOut.Operations), 0755)
-		if err != nil {
-			cli.Fatal(err)
-		}
-		t.Fatal("Incorrect assembly generated logs outputed to asmlog")
-	}
 }
