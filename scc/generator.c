df483f1dabbfc508f77ba9ac16c3b769e2f59c1f
vx-clutch <owestness@gmail.com>
2024-09-27 22:26:04 -0400

    save

---
+++ scc/generator.c
@@ -0,0 +1,16 @@
+/* generator.c: defines generator() for c, cc, etc
+    Copyright (C) 2024-2024 Silly Software Foundation.
+
+This file is part of SCC.
+
+SCC is free software; you can redistribute it and/or modify it under
+the terms of the BSD 3-Clause License as shown in the LICENCE file.
+
+SCC is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warrenty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the BSD 3-Clause Licence
+for more details.
+
+You should have received a copy of the BSD 3-Clause
+along with SCC; see the file LICENCE. If not see
+<https://raw.githubusercontent.com/sillysoftware/scc/refs/heads/master/LICENSE> */
