45e1eee1f0ace19afad4ca5b5798f325104bfc21
vx-clutch <owestness@gmail.com>
2024-10-10 20:10:29 -0400

    save

--- scc/main.cc
+++ scc/main.cc
@@ -24,14 +24,51 @@ along with SCC; see the file LICENCE. If not see
 #include <vector>
 
 #ifndef EXT
-#define EXT {""}
+#define EXT {""} /* set in CMakeLists.txt */
 #endif
 
 struct pflags {
     bool help {false};
     bool repl {false};
+    bool assembly {false};
+    bool verbose {false};
+    bool expand {false};
+    bool obj {false};
     std::optional<std::string> outfile;
 };
+
+pflags parse_args(const std::vector<std::string>& argv) {
+    pflags flags;
+    for (size_t i = 1; i < argv.size(); ++i) {
+        const std::string& arg = argv[i];
+        if (arg == "--help" || arg == "-h") {
+            flags.help = true;
+        } 
+        else if (arg == "--repl" || arg == "-R") {
+            flags.repl = true;
+        } 
+        else if (arg == "-S") {
+            flags.assembly = true;
+        }
+        else if (arg == "-E") {
+            flags.expand = true;
+        }
+        else if (arg == "-c") {
+            flags.obj = true;
+        }
+        else if (arg == "-o") {
+            if (i + 1 < argv.size()) {
+                flags.outfile = argv[i + 1];
+                i++;
+            } else {
+                error("missing filename after '-o'");
+                return flags;
+            }
+        }
+    }
+
+    return flags;
+}
 
 int main(int argc, char *argv[]) {
     std::vector<std::string> args;
@@ -41,9 +78,17 @@ int main(int argc, char *argv[]) {
     if (argc < 2) {
         fatal_error("no input files");
     }
+    pflags flags = parse_args(args);
+    if (flags.help) {
+        std::string helptxt = "Usage: scc [options] files...\n\nOptions:\n\n  -h, --help        Prints out the help and exit.\n  -R, --repl        Enables the REPL.\n  -o                Sets the output file.\n  -S                Compile only; do not assemble or link.\n  -E                Preproccess only; do not compile, assemble or link.\n  -c                Compile and assemble, but do not link.";
+        helptxt += "\n\nIf any bugs are found during use of this software report them to:\n<https://github.com/sillysoftware/scc/issues>";
+        std::cout << helptxt << std::endl;
+        exit(0);
+    }
     std::vector<std::string> ext = EXT;
     int found = 0;  
-    for (int i = 1; i < argc; i++) {
+    int kargc = argc;
+    for (int i = 1; i < kargc; i++) {
         auto carg = args[i];
         for (const auto &str : ext) {
             if (carg.length() >= str.length() && 
@@ -55,8 +100,14 @@ int main(int argc, char *argv[]) {
         }
     }
     if (found == 0) {  
-        fatal_error("file format not supported");
+        error("file format not supported");
     }
-    /* parser argv on ext */
+    if (flags.repl) {
+        std::cout << "REPL mode activated.\n";
+    }
+
+    if (flags.outfile) {
+        std::cout << "Output file: " << *flags.outfile << '\n';
+    }
     return 0;
 }
