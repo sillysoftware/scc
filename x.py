c3d741afd89e6f5c640af94b7f3ac84f5c0e5886
vx-clutch <owestness@gmail.com>
2024-11-20 21:52:39 -0500

    save

--- x.py
+++ x.py
@@ -1,13 +1,33 @@
-prog = "3 4 +"
+prog = "5 7 / 2 + ."
 
 stack = []
+segment = {}
 
-toks = prog.split()
+toks = iter(prog.split())
 for tok in toks:
-    if tok != "+":
+    if tok.isdigit():
         stack.append(int(tok))
+    elif tok.isalpha() and tok not in segment:
+        segment[str(tok)] = next(toks, 0)
+    elif tok.isalpha() and tok in segment:
+        stack.append(segment[str(tok)])
+    elif tok == ".":
+        print(stack)
     elif tok == "+":
         lhs = stack.pop(0)
         rhs = stack.pop(0)
         stack.append(lhs + rhs)
-print(stack)
+    elif tok == "-":
+        lhs = stack.pop(0)
+        rhs = stack.pop(0)
+        stack.append(lhs - rhs)
+    elif tok == "*":
+        lhs = stack.pop(0)
+        rhs = stack.pop(0)
+        stack.append(lhs * rhs)
+    elif tok == "/":
+        lhs = stack.pop(0)
+        rhs = stack.pop(0)
+        stack.append(lhs / rhs)
+
+print(segment)
